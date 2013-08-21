640a136c69c726c317593e6bce7efab447fdb244
Hexiyou <hackkey@qq.com>
2013-08-22 02:34:25 +0800

    添加数据库SQL备份文件

--- data/cache/setting.php
+++
@@ -1,196 +0,0 @@
-<?php
-return array (
-  'site_name' => '中华处方网',
-  'meta_description' => 'tipask最强问答程序',
-  'meta_keywords' => 'php问答系统,百度知道程序',
-  'cookie_domain' => '',
-  'cookie_pre' => 'tp_',
-  'seo_prefix' => '',
-  'seo_suffix' => '.html',
-  'date_format' => 'Y/m/d',
-  'time_format' => 'H:i',
-  'time_offset' => '8',
-  'time_diff' => '0',
-  'site_icp' => '',
-  'site_statcode' => '',
-  'allow_register' => '1',
-  'access_email' => '',
-  'censor_email' => '',
-  'censor_username' => '',
-  'maildefault' => 'tipask@domain.com',
-  'mailsend' => '1',
-  'mailserver' => 'smtp.domain.com',
-  'mailport' => '25',
-  'mailauth' => '0',
-  'mailfrom' => 'tpask <tipask@domain.com>',
-  'mailauth_username' => 'tipask@domain.com',
-  'mailauth_password' => '',
-  'maildelimiter' => '0',
-  'mailusername' => '1',
-  'mailsilent' => '0',
-  'credit1_register' => '20',
-  'credit2_register' => '20',
-  'credit1_login' => '2',
-  'credit2_login' => '0',
-  'credit1_ask' => '5',
-  'credit2_ask' => '0',
-  'credit1_answer' => '2',
-  'credit2_answer' => '0',
-  'credit1_message' => '-1',
-  'credit2_message' => '0',
-  'credit1_adopt' => '5',
-  'credit2_adopt' => '2',
-  'list_indexnosolve' => '5',
-  'list_indexcommend' => '5',
-  'list_indexreward' => '5',
-  'list_indexnote' => '5',
-  'list_indexallscore' => '5',
-  'list_indexweekscore' => '5',
-  'list_default' => '20',
-  'rss_ttl' => '60',
-  'code_register' => '0',
-  'code_login' => '0',
-  'code_ask' => '0',
-  'code_message' => '0',
-  'passport_type' => '0',
-  'passport_open' => '0',
-  'passport_key' => '',
-  'passport_client' => '',
-  'passport_server' => '',
-  'passport_login' => 'login.php',
-  'passport_logout' => 'login.php?action=quit',
-  'passport_register' => 'register.php',
-  'passport_expire' => '3600',
-  'passport_credit1' => '0',
-  'passport_credit2' => '0',
-  'overdue_days' => '60',
-  'ucenter_open' => '0',
-  'ucenter_url' => '',
-  'ucenter_ip' => '',
-  'ucenter_password' => '',
-  'ucenter_ask' => '1',
-  'ucenter_answer' => '1',
-  'seo_on' => '1',
-  'seo_title' => '',
-  'seo_keywords' => '',
-  'seo_description' => '',
-  'seo_headers' => '',
-  'notify_mail' => '0',
-  'notify_message' => '1',
-  'tpl_dir' => 'skin1',
-  'verify_question' => '0',
-  'index_life' => '1',
-  'msgtpl' => 'a:4:{i:0;a:2:{s:5:"title";s:36:"您的问题{wtbt}有了新回答！";s:7:"content";s:51:"你在{wzmc}上的提出的问题有了新回答！";}i:1;a:2:{s:5:"title";s:54:"恭喜，您对问题{wtbt}的回答已经被采纳！";s:7:"content";s:42:"你在{wzmc}上的回答内容被采纳！";}i:2;a:2:{s:5:"title";s:78:"抱歉，您的问题{wtbt}由于长时间没有处理，现已过期关闭！";s:7:"content";s:69:"您的问题{wtbt}由于长时间没有处理，现已过期关闭！";}i:3;a:2:{s:5:"title";s:42:"您对{wtbt}的回答有了新的评分！";s:7:"content";s:36:"您的回答{hdnr}有了新评分！";}}',
-  'allow_outer' => '2',
-  'stopcopy_on' => '0',
-  'stopcopy_allowagent' => 'webkit
-opera
-msie
-compatible
-baiduspider
-google
-soso
-sogou
-gecko
-mozilla',
-  'stopcopy_stopagent' => '',
-  'stopcopy_maxnum' => '60',
-  'editor_wordcount' => 'false',
-  'editor_elementpath' => 'false',
-  'editor_toolbars' => 'insertbuy,insertreply,insertvideo,inserthidezone,FullScreen,Source,Undo,Redo,RemoveFormat,|,Bold,Italic,FontSize,FontFamily,ForeColor,|,InsertImage,attachment,Emotion,Map,|,JustifyLeft,JustifyCenter,JustifyRight,|,HighlightCode',
-  'gift_range' => 'a:3:{i:0;s:2:"50";i:50;s:3:"100";i:100;s:3:"300";}',
-  'usernamepre' => 'tipask_',
-  'usercount' => '0',
-  'sum_onlineuser_time' => '30',
-  'sum_category_time' => '60',
-  'del_tmp_crontab' => '1440',
-  'allow_credit3' => '-10',
-  'apend_question_num' => '15',
-  'time_friendly' => '1',
-  'auth_key' => '3Ae81ubz1m8i7H62fWeddA0M6v6E4W3Jaz6S7Y2Y1J2Ufc2s8vcK0SdB4KeqdN4u',
-  'admin_email' => 'webmaster@domain.com',
-  'allow_fulltext	1
-' => '1',
-  'question_share' => '',
-  'allow_expert' => '0',
-  'seo_index_title' => '全球最大的中医处方点评分享网站',
-  'seo_index_keywords' => '全球最大的中医处方点评分享网站',
-  'seo_index_description' => '全球最大的中医处方点评分享网站',
-  'seo_question_title' => 'tipask',
-  'seo_question_keywords' => 'tipask',
-  'seo_question_description' => 'tipask',
-  'seo_category_title' => 'tipask',
-  'seo_category_keywords' => 'tipask',
-  'seo_category_description' => 'tipask',
-  'allow_fulltext' => '1',
-  'list_hot_words' => '神经内科
-心血管内科
-肾内科
-血液内科
-免疫科
-内分泌科
-健康饮食
-生活习惯
-人体常识
-戒烟戒酒
-中医内科
-中医外科
-中医妇科
-中医儿科
-中医保健科
-针灸按摩科
-中医骨伤科
-妇科
-产科
-妇幼保健
-不孕不育
-计划生育
-乳腺疾病
-
-普外科
-骨科
-神经外科
-心胸外科
-泌尿外科
-显微外科
-器官移植
-麻醉科
-神经内科
-心血管内科
-肾内科
-血液内科
-免疫科
-内分泌科
-健康饮食
-生活习惯
-人体常识
-戒烟戒酒
-中医内科
-中医外科
-中医妇科
-中医儿科
-中医保健科
-针灸按摩科
-中医骨伤科
-妇科
-产科
-妇幼保健
-不孕不育
-计划生育
-乳腺疾病
-
-普外科
-骨科
-神经外科
-心胸外科
-泌尿外科
-显微外科
-器官移植
-麻醉科
-',
-  'hot_words' => 'a:65:{i:0;a:2:{s:1:"w";s:9:"神经内科";s:3:"qid";i:0;}i:1;a:2:{s:1:"w";s:11:"心血管内科";s:3:"qid";i:0;}i:2;a:2:{s:1:"w";s:7:"肾内科";s:3:"qid";i:0;}i:3;a:2:{s:1:"w";s:9:"血液内科";s:3:"qid";i:0;}i:4;a:2:{s:1:"w";s:7:"免疫科";s:3:"qid";i:0;}i:5;a:2:{s:1:"w";s:9:"内分泌科";s:3:"qid";i:0;}i:6;a:2:{s:1:"w";s:9:"健康饮食";s:3:"qid";i:0;}i:7;a:2:{s:1:"w";s:9:"生活习惯";s:3:"qid";i:0;}i:8;a:2:{s:1:"w";s:9:"人体常识";s:3:"qid";i:0;}i:9;a:2:{s:1:"w";s:9:"戒烟戒酒";s:3:"qid";i:0;}i:10;a:2:{s:1:"w";s:9:"中医内科";s:3:"qid";i:0;}i:11;a:2:{s:1:"w";s:9:"中医外科";s:3:"qid";i:0;}i:12;a:2:{s:1:"w";s:9:"中医妇科";s:3:"qid";i:0;}i:13;a:2:{s:1:"w";s:9:"中医儿科";s:3:"qid";i:0;}i:14;a:2:{s:1:"w";s:11:"中医保健科";s:3:"qid";i:0;}i:15;a:2:{s:1:"w";s:11:"针灸按摩科";s:3:"qid";i:0;}i:16;a:2:{s:1:"w";s:11:"中医骨伤科";s:3:"qid";i:0;}i:17;a:2:{s:1:"w";s:5:"妇科";s:3:"qid";i:0;}i:18;a:2:{s:1:"w";s:5:"产科";s:3:"qid";i:0;}i:19;a:2:{s:1:"w";s:9:"妇幼保健";s:3:"qid";i:0;}i:20;a:2:{s:1:"w";s:9:"不孕不育";s:3:"qid";i:0;}i:21;a:2:{s:1:"w";s:9:"计划生育";s:3:"qid";i:0;}i:22;a:2:{s:1:"w";s:9:"乳腺疾病";s:3:"qid";i:0;}i:23;a:2:{s:1:"w";s:1:"";s:3:"qid";i:0;}i:24;a:2:{s:1:"w";s:7:"普外科";s:3:"qid";i:0;}i:25;a:2:{s:1:"w";s:5:"骨科";s:3:"qid";i:0;}i:26;a:2:{s:1:"w";s:9:"神经外科";s:3:"qid";i:0;}i:27;a:2:{s:1:"w";s:9:"心胸外科";s:3:"qid";i:0;}i:28;a:2:{s:1:"w";s:9:"泌尿外科";s:3:"qid";i:0;}i:29;a:2:{s:1:"w";s:9:"显微外科";s:3:"qid";i:0;}i:30;a:2:{s:1:"w";s:9:"器官移植";s:3:"qid";i:0;}i:31;a:2:{s:1:"w";s:7:"麻醉科";s:3:"qid";i:0;}i:32;a:2:{s:1:"w";s:9:"神经内科";s:3:"qid";i:0;}i:33;a:2:{s:1:"w";s:11:"心血管内科";s:3:"qid";i:0;}i:34;a:2:{s:1:"w";s:7:"肾内科";s:3:"qid";i:0;}i:35;a:2:{s:1:"w";s:9:"血液内科";s:3:"qid";i:0;}i:36;a:2:{s:1:"w";s:7:"免疫科";s:3:"qid";i:0;}i:37;a:2:{s:1:"w";s:9:"内分泌科";s:3:"qid";i:0;}i:38;a:2:{s:1:"w";s:9:"健康饮食";s:3:"qid";i:0;}i:39;a:2:{s:1:"w";s:9:"生活习惯";s:3:"qid";i:0;}i:40;a:2:{s:1:"w";s:9:"人体常识";s:3:"qid";i:0;}i:41;a:2:{s:1:"w";s:9:"戒烟戒酒";s:3:"qid";i:0;}i:42;a:2:{s:1:"w";s:9:"中医内科";s:3:"qid";i:0;}i:43;a:2:{s:1:"w";s:9:"中医外科";s:3:"qid";i:0;}i:44;a:2:{s:1:"w";s:9:"中医妇科";s:3:"qid";i:0;}i:45;a:2:{s:1:"w";s:9:"中医儿科";s:3:"qid";i:0;}i:46;a:2:{s:1:"w";s:11:"中医保健科";s:3:"qid";i:0;}i:47;a:2:{s:1:"w";s:11:"针灸按摩科";s:3:"qid";i:0;}i:48;a:2:{s:1:"w";s:11:"中医骨伤科";s:3:"qid";i:0;}i:49;a:2:{s:1:"w";s:5:"妇科";s:3:"qid";i:0;}i:50;a:2:{s:1:"w";s:5:"产科";s:3:"qid";i:0;}i:51;a:2:{s:1:"w";s:9:"妇幼保健";s:3:"qid";i:0;}i:52;a:2:{s:1:"w";s:9:"不孕不育";s:3:"qid";i:0;}i:53;a:2:{s:1:"w";s:9:"计划生育";s:3:"qid";i:0;}i:54;a:2:{s:1:"w";s:9:"乳腺疾病";s:3:"qid";i:0;}i:55;a:2:{s:1:"w";s:1:"";s:3:"qid";i:0;}i:56;a:2:{s:1:"w";s:7:"普外科";s:3:"qid";i:0;}i:57;a:2:{s:1:"w";s:5:"骨科";s:3:"qid";i:0;}i:58;a:2:{s:1:"w";s:9:"神经外科";s:3:"qid";i:0;}i:59;a:2:{s:1:"w";s:9:"心胸外科";s:3:"qid";i:0;}i:60;a:2:{s:1:"w";s:9:"泌尿外科";s:3:"qid";i:0;}i:61;a:2:{s:1:"w";s:9:"显微外科";s:3:"qid";i:0;}i:62;a:2:{s:1:"w";s:9:"器官移植";s:3:"qid";i:0;}i:63;a:2:{s:1:"w";s:7:"麻醉科";s:3:"qid";i:0;}i:64;a:2:{s:1:"w";s:0:"";s:3:"qid";i:0;}}',
-  'top_cat' => '10',
-  'top_area' => '15',
-  'top_all' => '20',
-);
-?>
--- data/cache/statistics.php
+++
@@ -1,7 +0,0 @@
-<?php
-return array (
-  'solves' => '7',
-  'nosolves' => '19',
-  'bestadopt' => 26.92,
-);
-?>
